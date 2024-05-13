670ff694ca78ffcdfe24563e210087efc313d5a9
kaoru-528 <origami.kaoru@gmail.com>
2024-05-13 17:09:44 +0900

    fix some

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<https://sample-app-lhfg.onrender.com>.onrender.com'
+  host = 'https://sample-app-lhfg.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
